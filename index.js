ed472ae882006e6f1219a2255c10da87cbc7883c
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-07-13 19:11:28 +0900

    server modify

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var socket_io_1 = require("socket.io");
+var http_1 = require("http");
+var app = (0, express_1.default)();
+var http = new http_1.Server(app);
+var socket = new socket_io_1.default.Server(http);
+var PORT = process.env.PORT || 5000;
+socket.on('connection', function (socket) {
+    console.log('connect');
+});
+socket.on('yyy', function (data) {
+    console.log("type: ".concat(typeof data, "   data: ").concat(data.message));
+});
+http.listen(PORT, function () {
+    console.log('server listening. Port:' + PORT);
+});
