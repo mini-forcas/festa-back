961a4797064ef1e9106ef479580cea255f0c3a36
Kazutake.Yamamoto <kazutake.yamamoto@uzabase.com>
2023-07-20 19:20:18 +0900

    change index.ts

--- index.ts
+++ index.ts
@@ -1,41 +1,24 @@
-import express, { Request, Response, NextFunction } from "express";
-import socketio from "socket.io";
-import { Server } from "http";
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
-var app = express();
-const allowCrossDomain = function (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers"
-    // "Content-Type, Authorization, access_token"
-  );
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
 
-  // intercept OPTIONS method
-  if ("OPTIONS" === req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-app.use(allowCrossDomain);
-
-var http = new Server(app);
-const socket = new socketio.Server(http);
-const PORT = process.env.PORT || 5000;
-
-socket.on("connection", function (socket) {
+io.on("connection", (socket) => {
   console.log("connect");
+
+  socket.on("yyy", (reason) => {
+    console.log(`user disconnected. reason is ${reason}`);
+  });
 });
 
-socket.on("yyy", (data: { message: string }) => {
-  console.log(`type: ${typeof data}   data: ${data.message}`);
-});
-
-http.listen(PORT, function () {
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, function () {
   console.log("server listening. Port:" + PORT);
 });
