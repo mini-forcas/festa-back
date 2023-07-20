4e24314aa5463fd365afa479e36c1e38159cad12
Kazutake.Yamamoto <kazutake.yamamoto@uzabase.com>
2023-07-20 18:48:37 +0900

    change index.ts

--- index.ts
+++ index.ts
@@ -1,20 +1,41 @@
-import express from "express";
-import socketio from 'socket.io';
-import { Server } from 'http'
+import express, { Request, Response, NextFunction } from "express";
+import socketio from "socket.io";
+import { Server } from "http";
 
 var app = express();
+const allowCrossDomain = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers"
+    // "Content-Type, Authorization, access_token"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
+
 var http = new Server(app);
 const socket = new socketio.Server(http);
 const PORT = process.env.PORT || 5000;
 
-socket.on('connection',function(socket){
-    console.log('connect');
+socket.on("connection", function (socket) {
+  console.log("connect");
 });
 
-socket.on('yyy', (data: { message: string }) => {
+socket.on("yyy", (data: { message: string }) => {
   console.log(`type: ${typeof data}   data: ${data.message}`);
 });
 
-http.listen(PORT, function(){
-    console.log('server listening. Port:' + PORT);
+http.listen(PORT, function () {
+  console.log("server listening. Port:" + PORT);
 });
