5aac5b9976fb228aa5ebe735945af12b2e80e6a3
mixnuts <french0719@gmail.com>
2023-08-03 19:11:30 +0900

    add socket

---
+++ scheme.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Quizzes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    admin_id INT, 
+    question TEXT, 
+    correct_option_id INT, -- 正しい選択肢のID（OptionsテーブルのIDを参照）
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Options (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_id INT, -- QuizzesテーブルのIDを参照
+    option_text TEXT, -- 選択肢のテキスト
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (quiz_id) REFERENCES Quizzes(id) -- 外部キー制約
+);
